aef9ae1d9c0b561164bfaa35ad1d929e7d53f0a0
Glib Shemenkov <shemenkovglib@gmail.com>
2023-10-27 12:51:26 +0200

    feat:done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (bracketsConfig === undefined) {
+    return false;
+  }
+  let openBrackets = {};
+  let closeBrackets = {};
+  let stack = [];
+  bracketsConfig.forEach((el, index) => {
+    //ключ это закрытие ) свойство это открытие (
+    openBrackets[el[1]] = el[0];
+    closeBrackets[el[0]] = el[0];
+  });
+
+  str.split("").forEach((el) => {
+    if (closeBrackets[el]) {
+      if (closeBrackets[el] === openBrackets[el] && el === stack[0]) {
+        stack.shift();
+      } else stack.unshift(el);
+    } else if (
+      openBrackets.hasOwnProperty(el) &&
+      openBrackets[el] === stack[0]
+    ) {
+      stack.shift();
+    } else stack.unshift(el);
+  });
+
+  return stack.length === 0;
+};
